9b3c8a8e621fa369d6e167767ed1a417fff30311
andrwsalcdo <asalcedo07@gmail.com>
2018-05-29 21:20:49 +0200

    use ngOnInit lifecylce instead of constructor

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { User } from '../../models/User';
 
@@ -7,10 +7,12 @@ import { User } from '../../models/User';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   user: User;
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
     this.user = {
       firstName: 'john',
       lastName: 'doe',
