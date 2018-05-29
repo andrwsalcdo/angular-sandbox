9c2a25a91c68485957739a5cb3cfcfa08257a4f4
andrwsalcdo <asalcedo07@gmail.com>
2018-05-29 20:57:46 +0200

    update user component with new properties

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {}
+export class UserComponent {
+  user: User;
+
+  constructor() {
+    this.user = {
+      firstName: 'john',
+      lastName: 'doe',
+      age: 30,
+      address: {
+        street: '123 abc street',
+        city: 'city',
+        state: 'state'
+      }
+    };
+  }
+}
