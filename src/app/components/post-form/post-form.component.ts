7d4201d4d97137342f97089ffefae6b472d482ec
andrwsalcdo <asalcedo07@gmail.com>
2018-06-04 12:56:52 +0200

    event & output: post form emits event to add post to posts component

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { PostsService } from '../../services/posts.service';
 import { Post } from '../../models/Post';
 
@@ -8,6 +8,9 @@ import { Post } from '../../models/Post';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  post: Post;
+  @Output() newPost: EventEmitter<Post> = new EventEmitter();
+
   constructor(private postsService: PostsService) {}
 
   ngOnInit() {}
@@ -17,7 +20,7 @@ export class PostFormComponent implements OnInit {
       alert('please add text');
     } else {
       this.postsService.savePost({ title, body } as Post).subscribe(post => {
-        console.log(post);
+        this.newPost.emit(post);
       });
     }
   }
