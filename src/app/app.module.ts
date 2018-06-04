6f4999291831ec8d7ba532f1c2ead143b4ab18bc
andrwsalcdo <asalcedo07@gmail.com>
2018-06-04 11:53:26 +0200

    create Posts component & services boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 import { DataService } from './services/data.service';
+import { PostsService } from './services/posts.service';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    NavbarComponent,
+    PostsComponent
+  ],
   imports: [BrowserModule, FormsModule],
-  providers: [DataService],
+  providers: [DataService, PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
